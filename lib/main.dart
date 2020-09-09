b1acd07e97f1af66bc9291b466991e6e76810d74
rafaelbarbosatec <rafaelbarbosatec@gmail.com>
2020-09-09 10:15:16 -0300

    ajustes para web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:bonfire/bonfire.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:multibiomegame/maps/map1.dart';
 
 final tileSize = 32.0;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Flame.util.setLandscape(); //TODO Comment when running for web
-  await Flame.util.fullScreen(); //TODO Comment when running for web
+  if (!kIsWeb) {
+    await Flame.util.setLandscape(); //TODO Comment when running for web
+    await Flame.util.fullScreen(); //TODO Comment when running for web
+  }
   runApp(MyApp());
 }
 
